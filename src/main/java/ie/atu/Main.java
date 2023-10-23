7ae7f75d1dfd2ec5ee2f8757601663bb093bdf2f
Tamer Zraiq <g00425053@atu.ie>
2023-10-23 17:23:11 +0100

    Did lotta things

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,7 +2,9 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        Person tamerZraiq = new Person("Tamer", "Zraiq", 18);
-        tamerZraiq.displayInfo();
+        Person user = new Person();
+        user.getUserInput();
+        user.displayInfo();
+        user.getFirstName();
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -4,10 +4,15 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Person {
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
 
+    public Person(){
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
+    }
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -27,6 +32,31 @@ public class Person {
         this.age = scanner.nextInt();
 
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void displayInfo(){
         System.out.println("Name: " + firstName + " " + lastName + " Age: "+ age);
     }
