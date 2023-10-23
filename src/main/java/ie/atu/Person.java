7a92a7b3e2d126c780b822da6e16d9f9b9b15feb
Tamer Zraiq <g00425053@atu.ie>
2023-10-23 16:25:30 +0100

    Added method displayInfo

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -4,4 +4,8 @@ public class Person {
     String firstName;
     String lastName;
     int age;
+
+    public void displayInfo(){
+        System.out.println("Name: " + firstName + " " + lastName + "Age: "+ age);
+    }
 }
