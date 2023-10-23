ccab4d38a8fa2ecdc51123a474cad20cf591e883
Tamer Zraiq <g00425053@atu.ie>
2023-10-23 16:27:10 +0100

    Added constructor

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -5,6 +5,12 @@ public class Person {
     String lastName;
     int age;
 
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public void displayInfo(){
         System.out.println("Name: " + firstName + " " + lastName + "Age: "+ age);
     }
