6a83106b816d4b826b01a53a644a25a0a7b8295a
Tamer Zraiq <g00425053@atu.ie>
2023-10-23 16:41:31 +0100

    Removed default constructor. Added another method getUserInput.

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,22 +1,32 @@
 package ie.atu;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Person {
     String firstName;
     String lastName;
     int age;
 
-    public Person(){
-        this.firstName = "Tamer";
-        this.lastName = "Zraiq";
-        this.age = 18;
-    }
-    
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
+    public void getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter First Name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.println("Enter age: ");
+        this.age = scanner.nextInt();
+
+    }
     public void displayInfo(){
         System.out.println("Name: " + firstName + " " + lastName + " Age: "+ age);
     }
